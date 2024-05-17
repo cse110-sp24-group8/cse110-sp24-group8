4516258868fc46e2d06e02062ddfb8a93cd4e596
nicholas-ngyn <122708113+nicholas-ngyn@users.noreply.github.com>
2024-05-16 18:50:16 -0700

    CSS & Markdown added!

--- src/scripts/main/public/js/documentation.js
+++
@@ -1,5 +0,0 @@
-function convertMarkdown() {
-    var inputText = document.getElementById('markdown-input').value;
-    var htmlContent = marked(inputText);
-    document.getElementById('html-output').innerHTML = htmlContent;
-}
