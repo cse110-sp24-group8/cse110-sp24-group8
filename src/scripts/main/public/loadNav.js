f6bc6995f5058137d108ab2113320f2df22ff308
kevk <kevinku668833@gmail.com>
2024-05-19 04:37:11 -0700

    Buttons are dynamically added with js

--- src/scripts/main/public/loadNav.js
+++ src/scripts/main/public/loadNav.js
@@ -1,16 +1,13 @@
-
-function importSidebar {
-const navBar = document.querySelector('.sidebar');
-    {
-        fetch(`./src/scripts/main/public/sidebar.html`)
-        .then (res => {
-            if (res.ok)  {
+document.addEventListener('DOMContentLoaded', function() {
+    const navElement = document.querySelector('.sidebar');
+    console.log('Dom Content Loaded');
+    fetch('sidebar.html')
+        .then(res => {
+            if (res.ok) {
                 return res.text();
             }
         })
-
-        .then(sidebarSnippet => {
-            navBar.innerHTML = sidebarSnippet;
-        });
-    }
-};
+        .then(htmlNav => {
+            navElement.innerHTML = htmlNav;
+        })
+});
