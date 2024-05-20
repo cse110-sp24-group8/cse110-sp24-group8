907acaa6f5eb41f80482caa170c6128925c17b17
Brandon <b1luu@ucsd.edu>
2024-05-20 13:39:48 -0700

    text-editor

--- src/scripts/main/public/code-log.js
+++ src/scripts/main/public/code-log.js
@@ -1,4 +1,10 @@
-// scripts/code-log.js
+var simplemde;
+document.getElementById('add-log-button').addEventListener('click', function() {
+    openModal();
+    if (!simplemde) {
+        simplemde = new SimpleMDE({ element: document.getElementById("logContent") });
+    }
+});
 
 function openModal() {
     document.getElementById('addLogModal').style.display = 'block';
@@ -12,5 +18,3 @@ function addLog() {
     // Add your logic to add the log here
     closeModal();
 }
-
-document.getElementById('add-log-button').addEventListener('click', openModal);
