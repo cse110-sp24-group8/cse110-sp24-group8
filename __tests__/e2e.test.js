ea32ef4d8169847da1aa4a094c94bd8e3fead462
cheung-arthur <kac019@ucsd.edu>
2024-06-06 02:18:18 -0700

    Added preliminary button testing on dashboard

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -14,8 +14,24 @@ describe("Exhaustive E2E testing based on user flow for website.", () => {
       await page.click('#viewAllTasksButton');
       const taskListUrl = await page.url();
       expect(taskListUrl).toContain('html/task-list.html');
+      await page.click('.sideButton img[alt="Dashboard Icon"]');
     });
 
+    test("Test view code log button", async () => {
+      await page.waitForSelector('#viewCodeLogButton');
+      await page.click('#viewCodeLogButton');
+      const codeLogUrl = await page.url();
+      expect(codeLogUrl).toContain('html/code-log.html');
+      await page.click('.sideButton img[alt="Dashboard Icon"]');
+    });
+
+    test("Test view documentation button", async () => {
+      await page.waitForSelector('#viewDocumentationButton');
+      await page.click('#viewDocumentationButton');
+      const documentationUrl = await page.url();
+      expect(documentationUrl).toContain('html/documentation.html');
+      await page.click('.sideButton img[alt="Dashboard Icon"]');
+    });
   });
 
 
