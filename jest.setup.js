46218cc270bfe6b7e4842603aef95c8fe678c922
Ulises Salinas <usalinas595@gmail.com>
2024-06-08 18:32:25 -0700

    add babel to enable S Module syntax

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,24 @@ import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+// export const transform = {
+//   '^.+\\.js$': 'babel-jest',
+// };
+// export const transformIgnorePatterns = [
+//   '/node_modules/',
+// ];
+// export const setupFilesAfterEnv = ['./jest.setup.mjs'];
+// export const globals = {
+//   'babel-jest': {
+//     useESM: true,
+//   },
+// };
+// module.exports = {
+//   transform: {
+//     '^.+\\.js$': 'babel-jest',
+//   },
+//   transformIgnorePatterns: [
+//     '/node_modules/',
+//   ],
+//   setupFilesAfterEnv: ['./jest.setup.js'],
+// };
